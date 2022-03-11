9db329f32c3a1b0874df879290c0ef004cf203ea
aagraston <aagraston@gmail.com>
2022-03-11 14:33:40 -0800

    first test set up, written, and passed

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
+const testables = () => {
+  const capitalize = (stringToCaps) =>
+    stringToCaps.charAt(0).toUpperCase() + stringToCaps.slice(1)
 
+  return { capitalize }
+}
 
-
+export default testables
---
+++ src/js/index.test.js
@@ -0,0 +1,7 @@
+import Testables from './index'
+
+const testables = Testables()
+
+test('check for initial letter caps', () => {
+  expect(testables.capitalize('try this')).toMatch(/^[A-Z][a-zA-Z0-9\s]{1,19}$/)
+})
