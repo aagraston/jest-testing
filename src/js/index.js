c0cf6d7aff73d87c86f63038bdedb73d8b0a23f7
aagraston <aagraston@gmail.com>
2022-03-14 10:57:44 -0700

    calculator functions added

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,19 @@ const testables = () => {
     return stringArrayRev.join('')
   }
 
-  return { capitalize, reverseString }
+  const addition = (num1, num2) => num1 + num2
+  const subtraction = (num1, num2) => num1 - num2
+  const division = (num1, num2) => num1 / num2
+  const multiplication = (num1, num2) => num1 * num2
+
+  return {
+    capitalize,
+    reverseString,
+    addition,
+    subtraction,
+    division,
+    multiplication,
+  }
 }
 
 export default testables
--- src/js/index.test.js
+++ src/js/index.test.js
@@ -9,3 +9,19 @@ test('check for initial letter caps', () => {
 test('check for reversed string', () => {
   expect(testables.reverseString('Reverse this')).toMatch('siht esreveR')
 })
+
+test('check addition', () => {
+  expect(testables.addition(45, 50)).toBe(95)
+})
+
+test('check addition', () => {
+  expect(testables.subtraction(45, 50)).toBe(-5)
+})
+
+test('check addition', () => {
+  expect(testables.division(6, 2)).toBe(3)
+})
+
+test('check addition', () => {
+  expect(testables.multiplication(3, 6)).toBe(18)
+})
