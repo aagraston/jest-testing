35bac19abcbefe0aa8ac588b7240e3c0730fc15a
aagraston <aagraston@gmail.com>
2022-03-14 10:36:01 -0700

    reverse string test & function added

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,14 @@ const testables = () => {
   const capitalize = (stringToCaps) =>
     stringToCaps.charAt(0).toUpperCase() + stringToCaps.slice(1)
 
-  return { capitalize }
+  const reverseString = (stringToRev) => {
+    const stringArray = stringToRev.split('')
+    const stringArrayRev = stringArray.reverse()
+    console.log(stringArrayRev)
+    return stringArrayRev.join('')
+  }
+
+  return { capitalize, reverseString }
 }
 
 export default testables
--- src/js/index.test.js
+++ src/js/index.test.js
@@ -5,3 +5,7 @@ const testables = Testables()
 test('check for initial letter caps', () => {
   expect(testables.capitalize('try this')).toMatch(/^[A-Z][a-zA-Z0-9\s]{1,19}$/)
 })
+
+test('check for reversed string', () => {
+  expect(testables.reverseString('Reverse this')).toMatch('siht esreveR')
+})
